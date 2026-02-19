0101194fe3bbc6a20fec79986b92c07f671130a5
shafiqahmed-786 <shafiqahmed0673@gmail.com>
2026-02-19 15:10:52 +0530

    Fix production API base URL

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,7 +1,18 @@
 import axios from "axios";
 
+/*
+  In production (Vercel), this will use:
+  REACT_APP_API_URL
+
+  Locally, it will fall back to:
+  http://127.0.0.1:5000/api
+*/
+
+const API_BASE =
+  process.env.REACT_APP_API_URL || "http://127.0.0.1:5000/api";
+
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL: API_BASE,
 });
 
 export const uploadFile = (file) => {
@@ -20,5 +31,5 @@ export const extractData = (fileId) => {
 };
 
 export const downloadExcel = (fileId) => {
-  return `${API.defaults.baseURL}/download/${fileId}`;
+  return `${API_BASE}/download/${fileId}`;
 };
